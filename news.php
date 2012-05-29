6cd8ca7ec2de226b1df82adf4aa7504d3b731574
Tom Kay <oridan82@gmail.com>
2012-05-29 11:10:55 +0000

    fix image link on RSS

--- news.php
+++ news.php
@@ -98,7 +98,7 @@ class module_NewsRSS extends uDataModule {
 			$crop = (strlen($row['text']) > 100) ? substr($row['text'],0,100).'...' : '';
 			$link = htmlentities('http://'.$dom.$obj->GetURL(array('news_id'=>$row['news_id'])));
 			$img = '';
-			if ($row['image']) $img = "\n".'  <media:thumbnail width="150" height="150" url="'.htmlentities('http://'.$dom.$this->GetImageLinkFromTable('image','news','news_id',$row['news_id'],150,150)).'"/>';
+			if ($row['image']) $img = "\n".'  <media:thumbnail width="150" height="150" url="'.htmlentities('http://'.$dom.$this->GetImageLink('image',$row['news_id'],150,150)).'"/>';
 			$updated = date('r',strtotime($row['time']));
 			if (!$pubDate || (strtotime($row['time']) > $pubDate)) $pubDate = strtotime($row['time']);
 			$items .= <<<FIN
